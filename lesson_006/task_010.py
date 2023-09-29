d108e0bdb941471020c8ef792e305478a9f41335
Vladislav <chenskiy.vlad@mail.ru>
2023-09-29 19:33:09 +0300

    hw7

--- lesson_006/task_010.py
+++ lesson_006/task_010.py
@@ -7,3 +7,19 @@
 1 до 8 - координаты 8 ферзей. Если ферзи не бьют друг друга верните истину, а если
 бьют - ложь.
 '''
+
+
+def check_queens(x, y):
+    n = 8
+    for i in range(n):
+        for j in range(i + 1, n):
+            if x[i] == x[j] or y[i] == y[j] or abs(x[i] - x[j]) == abs(y[i] - y[j]):
+                return False
+    return True
+
+
+x_coords = [8, 1, 2, 3, 4, 5, 6, 7]
+y_coords = [8, 3, 1, 7, 5, 2, 4, 6]
+
+print(check_queens(x_coords, y_coords))
+
--- lesson_006/task_011.py
+++ lesson_006/task_011.py
@@ -3,3 +3,25 @@
 для случайной расстановки ферзей в задаче выше. Проверяйте различный случайные
 варианты и выведите 4 успешных расстановки.
 '''
+import random
+
+
+def check_queens(x, y):
+    n = 8
+    for i in range(n):
+        for j in range(i + 1, n):
+            if x[i] == x[j] or y[i] == y[j] or abs(x[i] - x[j]) == abs(y[i] - y[j]):
+                return False
+    return True
+
+
+x_coords = list(range(1, 9))
+y_coords = list(range(1, 9))
+
+random.shuffle(x_coords)
+random.shuffle(y_coords)
+
+print(x_coords)
+print(y_coords)
+
+print(check_queens(x_coords, y_coords))
